3857639010f3f76b309f11e015f66f6d662a6d2e
Shreya2k05 <shreyaguptaa1408@gmail.com>
2026-03-30 13:15:16 +0530

    feat: implement CRUD operations for Group, Lead, and User routers

--- backend/routers/UserRouter.js
+++ backend/routers/UserRouter.js
@@ -13,123 +13,123 @@ router.post('/add', (req, res) => {
     console.log(req.body);
 
     new Model(req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err); 
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 
 });
 
 // getall
 router.get('/getall', (req, res) => {
     Model.find()
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err); 
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 //getbyemail
 // : denotes url parameter
 router.get('/getbyemail/:email', (req, res) => {
     Model.findOne({ email: req.params.email })
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err); 
-    });
-});  
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // getbycity
 router.get('/getbycity/:city', (req, res) => {
     Model.find({ city: req.params.city })
-    .then((result) => 
-        {res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err); 
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 
 // getbyid
 router.get('/getbyid/:id', (req, res) => {
     Model.findById(req.params.id)
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err); 
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 // delete
 router.delete('/delete/:id', (req, res) => {
     Model.findByIdAndDelete(req.params.id)
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 // update
 router.put('/update/:id', (req, res) => {
     Model.findByIdAndUpdate(req.params.id, req.body)
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/authenticate', (req, res) => {
     const { email, password } = req.body;
 
     Model.findOne({ email, password })
-    .then((result) => {
+        .then((result) => {
 
-        // if login is successful
-        if (result) {
+            // if login is successful
+            if (result) {
 
-            const {_id, email} = result;
+                const { _id, email } = result;
 
-            jwt.sign(
-                { _id, email }, 
-                process.env.JWT_SECRET,  
-                { expiresIn: '1h' },
-                (err, token) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).json(err);
-                    }else{
-                        res.status(200).json({ token });
+                jwt.sign(
+                    { _id, email },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '1h' },
+                    (err, token) => {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).json(err);
+                        } else {
+                            res.status(200).json({ token });
+                        }
                     }
-                }
-            )
+                )
 
-        }else{
-            //if login fails
-            res.status(403).json({ message: 'Invalid credentials' });
-        }
-       
+            } else {
+                //if login fails
+                res.status(403).json({ message: 'Invalid credentials' });
+            }
 
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 
 
 });
